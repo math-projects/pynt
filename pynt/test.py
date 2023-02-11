309a8c33fb0eb9e057d1eaa625ecbbebd44bd045
Masum <billalmasum93@gmail.com>
2023-02-11 23:59:09 +0600

    added poetry, pre-commit and dummy module

---
+++ pynt/test.py
@@ -0,0 +1,7 @@
+"""test"""
+
+
+def add(first: int, second: int) -> int:
+    """sum"""
+
+    return first + second
---
+++ tests/test_add.py
@@ -0,0 +1,9 @@
+"""test add"""
+
+from pynt.test import add
+
+
+def test_add():
+    """test add"""
+
+    assert add(2, 3) == 5
