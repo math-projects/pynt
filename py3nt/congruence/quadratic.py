bbd2e01eacb8ef6d2bd6eec95c1c429f0d1bf9fb
Masum Billal <billalmasum93@gmail.com>
2023-03-09 19:20:38 +0600

    added sympy and removed brent pollard's rho

--- py3nt/congruence/quadratic.py
+++ py3nt/congruence/quadratic.py
@@ -1,40 +1,32 @@
 """Quadratic residues and symbols"""
 
 
-from py3nt.numbers.integer import Integer
+from sympy.ntheory import jacobi_symbol, legendre_symbol
 
 
 def legendre(a: int, p: int) -> int:
     """Calculate Legendre symbol (a/p).
     p must be a prime.
 
-    :param a: Integer in Legendre symbol.
+    :param a: An integer.
     :type a: ``int``
-    :param p: Modulus in Legendre symbol.
+    :param p: A prime.
     :type p: ``int``
     :return: Legendre symbol (a/n). One of -1, 0 or 1.
     :rtype: ``int``
     """
 
-    if (a % p) == 0:
-        return 0
-
-    remainder = pow(Integer(a), exp=(p - 1) >> 1, mod=p)
-
-    if ((remainder - 1) % p) == 0:
-        return 1
-
-    return -1
+    return legendre_symbol(a=a, p=p)
 
 
 def jacobi(a: int, n: int) -> int:
     """Calculate Jacobi symbol (a/n).
     n must be a positive odd number.
 
-    :param a: Integer in Jacobi symbol.
+    :param a: An integer.
     :type a: ``int``
-    :param n:
-    :type n: int
+    :param n: A positive odd integer.
+    :type n: ``int``
     :raises ValueError: If n is not positive or odd.
     :return: Jacobi symbol (a/n). One of -1, 0 or 1.
     :rtype: ``int``
@@ -46,25 +38,4 @@ def jacobi(a: int, n: int) -> int:
     if (n & 1) == 0:
         raise ValueError(f"{n} is not odd.")
 
-    res = 1
-    a %= n
-
-    while a != 0:
-        while (a & 1) == 0:
-            a >>= 1
-            rem = n % 8
-
-            if rem == 3 or rem == 5:
-                res = -res
-
-        a, n = n, a
-
-        if (a % 4) == (n % 4) == 3:
-            res = -res
-
-        a %= n
-
-    if n == 1:
-        return res
-
-    return 0
+    return jacobi_symbol(m=a, n=n)
