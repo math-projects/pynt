73f4a45ba508223730278eddcc11a689658af69b
Masum Billal <billalmasum93@gmail.com>
2023-03-18 18:57:26 +0600

    calculate and generate binomial coefficients

--- tests/polynomial/test_binomial.py
+++ tests/polynomial/test_binomial.py
@@ -3,7 +3,11 @@
 
 import pytest
 
-from py3nt.polynomial.binomial import homogeneous_binomial
+from py3nt.polynomial.binomial import (
+    binomial_coefficient,
+    generate_binomial_coefficients,
+    homogeneous_binomial,
+)
 
 
 def test_homogeneous_binomial() -> None:
@@ -19,3 +23,29 @@ def test_homogeneous_binomial() -> None:
 
     with pytest.raises(ValueError):
         homogeneous_binomial(a=2, b=1, n=0)
+
+
+def test_binomial_coefficient() -> None:
+    """Test Binomial Coefficient"""
+
+    assert binomial_coefficient(n=10, k=5) == 252
+    assert binomial_coefficient(n=4, k=10) == 0
+    assert binomial_coefficient(n=100, k=0) == 1
+    assert binomial_coefficient(n=7, k=2) == 21
+
+    with pytest.raises(ValueError):
+        binomial_coefficient(n=-1, k=10)
+
+
+def test_generate_binomial_coefficient() -> None:
+    """Test binomial coefficient generation"""
+
+    nCk = generate_binomial_coefficients(n=10)
+
+    assert nCk[0, 0] == 1
+    assert nCk[1][0] == 1
+    assert nCk[1][1] == 1
+    assert nCk[1][10] == 0
+    assert nCk[10][4] == 210
+    assert nCk[10, 5] == 252
+    assert nCk[10, 10] == 1
